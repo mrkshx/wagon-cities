408a5cdf9ef03f9baf19db64c1fa2802a8ee15a6
Markus Mengel <markus.mengelhx@gmail.com>
2020-06-12 14:11:04 +0200

    created active_city component

---
+++ src/containers/active_city.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { selectCity } from '../actions';
+
+
+class ActiveCity extends Component {
+  componentWillMount() {
+    this.props.setCities();
+  }
+
+  renderList = () => {
+    return (
+      <div>
+        <h2>{selectCity.name}</h2>
+        <p>{selectCity.address}</p>
+        <img src={`https://kitt.lewagon.com/placeholder/cities/${selectCity.slug}`} alt="" />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="active-city">
+        {this.renderList()}
+      </div>
+    );
+  }
+}
+
+
+function mapStateToProps(state) {
+  return { selectCity: state.selectCity };
+}
+
+export default connect(mapDispatchToProps)(ActiveCity);
